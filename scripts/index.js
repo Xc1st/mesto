4c28d4e4e906b066f46099dea29890314d394486
Daniil <dan.ustinoff2011@yandex.ru>
2023-03-19 22:18:56 +0300

    added 4

--- scripts/index.js
+++ scripts/index.js
@@ -2,18 +2,19 @@ const popup = document.querySelector(".popup");
 const buttonClosePopUpProfile = document.querySelector(".popup__close-icon");
 const buttonEditProfile = document.querySelector(".profile__edit-button");
 const formEditProfile = document.querySelector(".popup__form");
-const buttonSubmitProfile = document.querySelector(".popup__save");
 const profileTitle = document.querySelector(".profile__title");
 const profileSubtitle = document.querySelector(".profile__subtitle");
 const inputName = document.querySelector(".popup__text_type_name");
 const inputLink = document.querySelector(".popup__text_type_about");
 
 const openPopup = function () {
-    popup.classList.add("popup_is-opened");
+    popup.classList.add("popup_opened");
+    inputName.value = profileTitle.textContent;
+    inputLink.value = profileSubtitle.textContent;
 }
 
 const closePopup = function () {
-    popup.classList.remove("popup_is-opened");
+    popup.classList.remove("popup_opened");
 }
 
 function handleFormSubmitProfile(evt) {
@@ -23,8 +24,6 @@ function handleFormSubmitProfile(evt) {
     closePopup();
 }
 buttonEditProfile.addEventListener("click", () => {
-    inputName.value = profileTitle.textContent;
-    inputLink.value = profileSubtitle.textContent;
     openPopup();
 });
 
